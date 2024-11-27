3d5ef0ed862033bbff8adea8067368cec47c3d9c
behan05 <=>
2024-11-28 02:31:28 +0530

    contact page implemented

---
+++ client/public/scripts/contact.js
@@ -0,0 +1,48 @@
+function menuButton() {
+    const openMenuIcon = document.querySelector(".openMenu");
+    const closeMenuIcon = document.querySelector(".closeMenu");
+    const header__menu = document.querySelector(".header__menu");
+
+    openMenuIcon.addEventListener("click", (e) => {
+        closeMenuIcon.style.display = "block";
+        openMenuIcon.style.display = "none";
+        header__menu.style.display = "block";
+
+    })
+    closeMenuIcon.addEventListener("click", (e) => {
+        closeMenuIcon.style.display = "none";
+        openMenuIcon.style.display = "block";
+        header__menu.style.display = "none";
+
+    })
+}
+
+// GSAP Code.
+function headerGsap() {
+    gsap.from("#header__logo", {
+        x: -200,
+        duration: 1,
+        rotation: 360,
+
+    })
+    gsap.from(".header__menu ul li", {
+        y: -100,
+        stagger: 0.1
+    })
+}
+
+function contact() {
+    gsap.from("#contact", {
+        scale: 0,
+        opacity: 0,
+        duration: 1,
+    })
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const closeMenuIcon = document.querySelector(".closeMenu");
+    closeMenuIcon.style.display = "none";
+    menuButton();
+    headerGsap();
+    contact();
+})
